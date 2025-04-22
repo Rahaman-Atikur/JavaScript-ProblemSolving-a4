c8153a8aa21c5bdb7ada0f3204bf98349139616c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-22 19:20:53 +0100

    P4 solved but , is Object showed in course need to check

--- p4.js
+++ p4.js
@@ -1,19 +1,20 @@
-let person1 ={ name: "Rahul", gender: "male", age: 28 };
-let person2 ={ name: "Joya", gender: "female", age: 21 };
-
+let person2 = "Mozan";
+let person1 = { name: "bjoy", gender: "male", age: 32 };
 function validProposal(person1, person2) {
-    
-    if (typeof person1 !== Object || typeof person2 !== Object) {
+    if (!Object.is(person1, person2)) {
         return "Invalid";
     }
-    let ageDifference = Math.abs(person1['age']-person2['age']);
-    if (person1['gender'] == 'male' && person2['gender'] == 'female' && ageDifference >= 0 && ageDifference < 7) {
+    let ageDifference = Math.abs(person1['age'] - person2['age']);
+    if (person1['gender'] == 'male' && person2['gender'] == 'female' && ageDifference >= 0 && ageDifference <= 7) {
+        return true;
+    }
+    else if (person1['gender'] == 'female' && person2['gender'] == 'male' && ageDifference >= 0 && ageDifference <= 7) {
         return true;
     }
     else {
         return false;
     }
 }
-let result = validProposal(person1,person2);
+let result = validProposal(person1, person2);
 console.log(result);
 
