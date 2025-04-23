e53cd4af1d2dcd48078deed10e399450a600227d
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-23 08:35:53 +0100

    p4 soved, testing left

--- p4.js
+++ p4.js
@@ -1,7 +1,7 @@
-let person2 = "Mozan";
-let person1 = { name: "bjoy", gender: "male", age: 32 };
+let person1 = { name: "milon", gender: "male", age: 28 };
+let person2 = { name: "bjoy", gender: "male", age: 32 };
 function validProposal(person1, person2) {
-    if (!Object.is(person1, person2)) {
+    if(typeof person1!=="object" ||typeof person2!=="object"){
         return "Invalid";
     }
     let ageDifference = Math.abs(person1['age'] - person2['age']);
